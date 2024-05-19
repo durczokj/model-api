ee5d0669936bd95dd926d2bbc5c6dd1119079f46
Jakub Durczok <durczok.jakub@gmail.com>
2024-05-19 20:03:20 +0200

    Refactoring art 2

--- src/predict.py
+++ src/predict.py
@@ -14,3 +14,42 @@ class _Predictor:
     def __init__(
         self,
         expected_features,
+        model):
+        
+        self.expected_features = expected_features
+        self.model = model
+    
+    def __get_features(self, request):
+        features = {}
+        
+        for feature in self.expected_features:
+            value = request.args.get(feature, type=float)
+            if value is not None:
+                features[feature] = value
+        
+        missing_features = [feature for feature in self.expected_features if feature not in features.keys()]
+        print(self.expected_features)
+        print(features.values())
+        print(missing_features)
+        if len(missing_features) > 0:
+            raise ExpectedFeaturesNotProvidedError(missing_features)
+        
+        return features
+    
+    def __get_prediction(self, features):
+        freature_array = np.array(list(features.values()), ndmin = 2)    
+        return self.model.predict(freature_array)[0]
+    
+    def predict(self, request):
+        features = self.__get_features(request)
+        prediction = self.__get_prediction(features)
+        return self.__get_prediction(features)
+
+
+with open(config["MODEL_FILE_NAME"], 'rb') as f_in:
+    model = pickle.load(f_in)
+
+predictor = _Predictor(expected_features=config["EXPECTED_FEATURES"], model = model)
+
+def predict(request):
+    return predictor.predict(request)
